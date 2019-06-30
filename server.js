41579bc2f6bbb3213abac37a5467f25807a54741
unknown <mauro.hdz99@gmail.com>
2019-06-29 23:31:04 -0500

    Added handlebars boiler to server.js

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function(){
+    console.log("App listening on port: " + PORT);
+})
