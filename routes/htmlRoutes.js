08d1f125991ac5960ceac0ffb2a9b004f3352778
unknown <mauro.hdz99@gmail.com>
2019-07-03 11:16:33 -0500

    Added saved.handlebars file and route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
 module.exports = function(app) {
+    // example route
+app.get("/", function(req, res) {
+res.render("index",)
+
+});
+
+app.get("/", function(req, res) {
+res.render("saved",)
+
+})
+
+
 
 };
