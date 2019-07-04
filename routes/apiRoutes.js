d6e9a40541b8716170977e4a808319c116fc3490
unknown <mauro.hdz99@gmail.com>
2019-07-04 12:17:26 -0500

    Fixed scrape articles btn

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,31 +5,10 @@ const cheerio = require("cheerio");
 
 module.exports = function(app) {
 
-app.get("/scrape-articles", function() {
+app.get("/scrape-articles", function(req, res) {
 // When this route is hit, it is supposed to go scrape again, and delete the old articles
-    axios.get("https://finance.yahoo.com/news/").then(function(response) {
-
-const $ = cheerio.load(response.data);
-
-const results = [];
-
-$("h3").each(function(i, element, results) {
-
-        const title = $(element).text();
-
-       const link = $(element).children().attr("href");
-
-       results.push({
-           title: title, 
-           link:"https://finance.yahoo.com" + link
-        });
-
-
-});
-console.log(results)
-res.render("index", {article: results});
-
-});
+    res.redirect("/");
+    console.log("NEW ARTICLES SCRAPED!!!");
 });
 
 app.get("/clear", function() {
