5b7c48bf281bba5e1b477b69f117f02d07b81ba1
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-04 16:08:29 -0600

    changed brand name

--- client/src/components/jumbotron.js
+++ client/src/components/jumbotron.js
@@ -7,7 +7,7 @@ function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid mb-0">
   <div className="container">
-    <h1 className="display-4 text-center text-white">Welcome to Book-Finder!</h1>
+    <h1 className="display-4 text-center text-white">Welcome to Book-Tastic!</h1>
     <p className="lead text-center text-white">Search from our big selection of books.</p>
   </div>
 </div>
