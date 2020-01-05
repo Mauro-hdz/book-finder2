0207da66c5721d37c9c6a7de739aad726dcc9938
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-05 03:26:56 -0600

    controller under construction

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,20 +1,44 @@
-const db = require("../models/books");
+const Book = require("../models/books");
 
 module.exports = {
     findAll: function(req, res) {
-        
+        Book.findAll()
+        .then(response => {
+            console.log(response);
+            res.json(response);
+        })
+        .catch(err => {console.log('Error at findAll: ' + err)});
     },
 
     findOne: (req, res) => {
+        Book.findAll({
+            where: {
+                //some property to be determined
+            }
+        })
 
     },
 
     saveBook: function(req, res) {
-        
+        Book.create({
+            //properties of our model with requested info
+        })
+        .then(response => {
+            console.log(response, 'Book successfully destroyed');
+          })
+        .catch(err => {console.log('Error at saveBook: ' + err)});
     },
 
     deleteBook: function(req, res) {
-
+        Book.destroy({
+            where: {
+                //properties to look for to delete the object
+            }
+        })
+        .then(response => {
+            console.log(response, 'Book successfully deleted')
+        })
+        .catch(err => {console.log('Error at deleteBook: ' + err)})
     }
 
 };
